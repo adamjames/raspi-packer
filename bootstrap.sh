6e7ce6327a42d07e9655dafd3f870eddd202dd70
Adam James <adam@adamjames.me>
2023-08-21 00:47:42 +0100

    Rework file names and parameters, some improvements
    - Image names per preference
    - use_rpi_bootloader -> use_microboot
    - optionally lock root
    - use bsdtar everywhere
    - add template for arrpi
    - debug builds pass v flag
    - remove some extraneous settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,10 @@ github_keys="${GITHUB_KEYS}"
 git_user_name="${GIT_USER_NAME}"
 git_user_email="${GIT_USER_EMAIL}"
 pi4_alt_fstab="${PI4_ALT_FSTAB}"
-pi4_bootloader="${PI4_BOOTLOADER}"
+use_microboot="${USE_MICROBOOT}"
 cm4_usb="${CM4_USB}"
-lockdown_root="${LOCKDOWN_ROOT}"
+lock_root_account="${LOCK_ROOT_ACCOUNT}"
+enable_mac_hostname="${ENABLE_MAC_HOSTNAME}"
 
 # Recomended in https://wiki.archlinux.org/index.php/Chroot#Using_chroot
 # Doesn't seem to do much
@@ -32,10 +33,13 @@ fi
 echo 'nameserver 8.8.8.8' > /etc/resolv.conf;
 pacman -Syyu --noconfirm --needed
 
-if [ "$pi4_bootloader" = "true" ] ; then
+if [ "$use_microboot" = "false" ] ; then
+  echo 'Microboot support not requested, installing Pi kernel/firmware/bootloader...'
   pacman -R linux-aarch64 uboot-raspberrypi --noconfirm
   pacman -S linux-rpi raspberrypi-bootloader firmware-raspberrypi raspberrypi-firmware --noconfirm --needed
-fi
+else
+  echo 'Microboot support was requested.'
+  echo 'linux-aarch64 & uboot-raspberrypi are provided by default. Continuing...'
 
 # Set up localization https://wiki.archlinux.org/index.php/Installation_guide#Localization
 sed -i 's/#en_GB.UTF-8 UTF-8/en_GB.UTF-8 UTF-8/g' /etc/locale.gen
@@ -65,11 +69,13 @@ chmod +x /tmp/resizerootfs
 mv /tmp/resizerootfs /usr/sbin/
 systemctl enable resizerootfs.service
 
-# set up mac-host firstrun script
-mv /tmp/mac-host.service /etc/systemd/system
-chmod +x /tmp/mac-host
-mv /tmp/mac-host /usr/sbin/
-systemctl enable mac-host.service
+if [ "$enable_mac_hostname" = "true" ] ; then
+  # set up mac-host firstrun script
+  mv /tmp/mac-host.service /etc/systemd/system
+  chmod +x /tmp/mac-host
+  mv /tmp/mac-host /usr/sbin/
+  systemctl enable mac-host.service
+fi
 
 # Set Hostname
 # Normally we use hostnamectl, but that doesn't work in chroot
@@ -79,7 +85,7 @@ echo "${hostname}" > /etc/hostname
 # Install stuff
 pacman -S vim htop parted sudo --noconfirm --needed
 
-# Sometiems the network file is missing for some unknown reason
+# Sometimes the network file is missing for some unknown reason
 if [ ! -f "/etc/systemd/network/en.network" ] ; then
   echo 'Fixing eth -> en network file'
   touch /etc/systemd/network/en.network
@@ -106,7 +112,7 @@ systemctl enable systemd-resolved.service
 
 # disable password auth
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-#systemctl restart sshd
+systemctl restart sshd
 
 # enable color on
 # pacman
@@ -117,21 +123,6 @@ useradd -m "${username}"
 usermod -aG wheel "${username}"
 usermod -aG video "${username}"
 
-if [ "$lockdown_root" = "true" ] ; then
-  # delete default user alarm:alarm
-  userdel -r alarm
-  # disable root login root:root
-  # https://wiki.archlinux.org/index.php/Sudo#Disable_root_login
-  passwd -l root
-else
-  # add alarm to sudo if its not deleted
-  usermod -aG wheel "alarm"
-fi
-
-# copy the throttle script
-#mv /tmp/throttle.sh "/home/${username}/throttle.sh"
-#chmod +x "/home/${username}/throttle.sh"
-
 # Setup user ssh keys
 mkdir /home/"${username}"/.ssh
 touch "/home/${username}/.ssh/authorized_keys"
@@ -141,8 +132,26 @@ chmod go-w "/home/${username}"
 chmod 700 "/home/${username}/.ssh"
 chmod 600 "/home/${username}/.ssh/authorized_keys"
 
+
+if [ "$remove_alarm" = "true" ] ; then
+  userdel -r alarm
+else
+  # Add alarm to wheel so that it can use sudo
+  usermod -aG wheel "alarm"
+fi
+
+if [ "$lock_root_account" = "true" ] ; then
+  # disable root login root:root
+  # https://wiki.archlinux.org/index.php/Sudo#Disable_root_login
+  passwd -l root
+fi
+
 # Set up no-password sudo
-#echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
+echo '%wheel ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/wheel
+
+# copy the throttle script
+mv /tmp/throttle.sh "/home/${username}/throttle.sh"
+chmod +x "/home/${username}/throttle.sh"
 
 if [ "$pi4_alt_fstab" = "true" ] ; then
   echo 'setting up pi4 fstab'
